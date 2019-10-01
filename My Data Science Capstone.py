2124a95eb0af5b1b8659b47da723759f2d8c5108
alexschanne <56009921+alexschanne@users.noreply.github.com>
2019-10-01 01:23:58 -0400

    Add files via upload

---
+++ My Data Science Capstone.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1/> My Data Science Capstone 
+
+# This notebook will be where I'll execute my IBM Data Science Capstone Project to complete my certificate on Coursera. I will be applying the new skills I have acquired during the previous courses to showcase what I've learned.
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
