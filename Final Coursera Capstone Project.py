520b45798f4dfac83b4234c541c4aa93ffdfb4a3
alexschanne <56009921+alexschanne@users.noreply.github.com>
2019-10-12 02:43:16 -0400

    Add files via upload

---
+++ Final Coursera Capstone Project.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <H1> Coursera Capstone Project
+
+# This notebook will apply the skills and tools that I have learned during the IBM Coursera Data Science Certification. It will demonstrate my understanding of these tools and ability to use them to creatively solve complex problems.
+
+# To clarify the requirements of the project, I will restate the rubric here:
+
+# For Week 1:
+# <ul>
+#     <li>A description of the problem and a discussion of the background. (15 marks)</li>
+#     <li>A description of the data and how it will be used to solve the problem. (15 marks)</li>
+#     </ul>
+# 
+# For Week 2:
+# <ul>
+#     <li>A link to your Notebook on your Github repository, showing your code. (15 marks)</li>
+#     <li>A full report consisting of all of the following components (15 marks):
+#         <ul>
+#             <li>Introduction where you discuss the business problem and who would be interested in this project.</li>
+#             <li>Data where you describe the data that will be used to solve the problem and the source of the data.</li>
+#             <li>Methodology section which represents the main component of the report where you discuss and describe any exploratory data analysis that you did, any inferential statistical testing that you performed, if any, and what machine learnings were used and why.</li>
+#             <li>Results section where you discuss the results.</li>
+#             <li>Discussion section where you discuss any observations you noted and any recommendations you can make based on the results. </li>
+#             <li>Conclusion section where you conclude the report.</li>
+#         </ul>
+#     <li>Your choice of a presentation or blogpost. (10 marks)</li>
+#     </ul>
+
+# <H2>Introduction
+
+# <H4>The Problem</H4> 
+# This notebook will investigate the viability of opening a 24/7 diner in Philadelphia, Pennsylvania.
+# <p></p>
+# As a Philadelphia native, I love the city. But I am always struck by how many times people remark to me how early the city shuts down compared to its neighbor, New York. There are very few late night eateries, particularly near the University City district, where many students live and have expressed interest in late night options for after party hangouts or study fuel.
+
+# <H2>Methodology
+
+# <H4>DATA: This Notebook as a Solution</H4>
+# In this notebook, I will use data scraped from Wikipedia to identify neighborhoods in Philadelphia, PA. After which, I will create a dataframe and map the coordinates of the neighborhood. I will use the latitude and longitude to get venue data from Four Square to confirm or reject my hypothesis that University City is an viable neighborhood to open a 24/7 diner.
+
+# In[ ]:
+
+
+
+
